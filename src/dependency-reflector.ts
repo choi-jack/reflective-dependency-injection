24b226745a253fc35c56d8e7c0841773654b9b58
choi.jaehyeok <choi.jaehyeok@outlook.com>
2024-09-02 21:58:26 +0900

    feat: Change property key type

--- src/dependency-reflector.ts
+++ src/dependency-reflector.ts
@@ -3,7 +3,7 @@ import { MetadataReflector } from 'class-metadata';
 import { Dependency } from './dependency.js';
 import { DEPENDENCIES } from './metadata-keys.js';
 
-export function getOwnDependencies(target: object, propertyKey?: null | PropertyKey): null | ReadonlyArray<Dependency> {
+export function getOwnDependencies(target: object, propertyKey?: null | string | symbol): null | ReadonlyArray<Dependency> {
     return MetadataReflector
         .reflect(target, propertyKey)
         .getOwn(DEPENDENCIES);
